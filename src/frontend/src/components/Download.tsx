b340fb242c40307f3d933068ecb13faf9bb27124
KaraboMo <karabo.molemane@adaptit.com>
2024-03-18 10:45:20 +0200

    donload to folder withn Download and UI card

--- src/frontend/src/components/Download.tsx
+++ src/frontend/src/components/Download.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import CardHeader from "./CardHeader";
 const fs = window.require("fs");
 const path = window.require("path");
 
@@ -10,19 +11,30 @@ const Download = (props: any) => {
     console.log("Starting download...");
     toast("Starting download...");
 
-    const downloadFolder = path.join(process.env.HOME || "", "Downloads", "CWA-Installer", "components");
+    const downloadFolder = path.join(
+      process.env.HOME || "",
+      "Downloads",
+      "CWA-Installer",
+      "components"
+    );
 
     try {
+      // Create the directory if it doesn't exist
+      if (!fs.existsSync(downloadFolder)) {
+        fs.mkdirSync(downloadFolder, { recursive: true });
+      }
+
       for (let index = 0; index < props.selectedProducts.length; index++) {
         const product = props.selectedProducts[index];
         const fileName = `file${index + 1}.zip`;
-        const downloadUrl = "https://cwa-installer-assets-test.s3.eu-west-1.amazonaws.com/combinations/4320515af08e67230d58f349b40f2515516229a2/CWA-Installer.zip";
+        const downloadUrl =
+          "https://cwa-installer-assets-test.s3.eu-west-1.amazonaws.com/combinations/4320515af08e67230d58f349b40f2515516229a2/CWA-Installer.zip";
         const filePath = path.join(downloadFolder, fileName);
 
         console.log(`Downloading ${fileName} from ${downloadUrl}`);
         toast(`Downloading ${fileName} from ${downloadUrl}`);
         toast(`filePath ${filePath}`);
-        toast(`process.env.HOME: ${process.env.HOME}`)
+        toast(`process.env.HOME: ${process.env.HOME}`);
 
         const response = await fetch(downloadUrl);
         if (!response.ok) {
@@ -91,13 +103,43 @@ const Download = (props: any) => {
 
   return (
     <>
-      <button onClick={handleDownload}>Download Files</button>
-      <div>
-        {downloadProgress.map((progress: any, index: number) => (
-          <p key={index}>
-            File {index + 1} Download Progress: {progress}%
-          </p>
-        ))}
+      <div
+        className="card"
+        style={{
+          paddingLeft: "17rem !important",
+          paddingRight: "17rem !important",
+        }}
+      >
+        <div className="card-header">
+          <CardHeader
+            cardText={"Please wait while applications are being downloaded..."}
+          />
+          <div className="row"></div>
+        </div>
+        <div className="card-body">
+          <div className="card-title">
+            <div>
+              <div
+                className="pull-right text-right btn btn-outline-dark disabled"
+                style={{ float: "right", textAlign: "center" }}
+              >
+                <h2 className="ng-binding">30%</h2>
+                <h5>Overall download progress</h5>
+              </div>
+              <h5 className="card-title">Download</h5>
+            </div>
+          </div>
+          <div className="card-text">
+            <button onClick={handleDownload}>Download Files</button>
+            <div>
+              {downloadProgress.map((progress: any, index: number) => (
+                <p key={index}>
+                  File {index + 1} Download Progress: {progress}%
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
       {/* toaster */}
       <ToastContainer />
