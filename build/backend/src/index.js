ebb1b677ff4343c5a19873ce423a5199129bb3f9
KaraboMo <karabo.molemane@adaptit.com>
2024-03-26 10:04:28 +0200

    use ONE package.json and connct to backend via sequelize

--- build/backend/src/index.js
+++ build/backend/src/index.js
@@ -1,17 +1,46 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const sequelize_1 = require("sequelize");
 const dataRoutes_1 = __importDefault(require("./routes/dataRoutes"));
 const app = (0, express_1.default)();
 // Allow all CORS requests (not recommended for production)
 app.use((0, cors_1.default)());
-app.use('/api', dataRoutes_1.default);
+// Connect to PostgreSQL database
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME || "cwa_cloud_installer", process.env.DB_USERNAME || "postgres", process.env.DB_PASSWORD || "docker", {
+    dialect: "postgres",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT) || 5432,
+});
+// Test the database connection
+function testDatabaseConnection() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield sequelize.authenticate();
+            console.log("Connection to the database has been established successfully.");
+        }
+        catch (error) {
+            console.error("Unable to connect to the database:", error);
+        }
+    });
+}
+testDatabaseConnection();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+app.use("/api", dataRoutes_1.default);
 // ts-node src/backend/src/index.ts
--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,17 +1,44 @@
-import express from 'express';
-import cors from 'cors';
-import dataRoutes from './routes/dataRoutes';
+import express from "express";
+import cors from "cors";
+import { Sequelize } from "sequelize";
+import dataRoutes from "./routes/dataRoutes";
 
 const app = express();
 
 // Allow all CORS requests (not recommended for production)
 app.use(cors());
 
-app.use('/api', dataRoutes);
+// Connect to PostgreSQL database
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "cwa_cloud_installer",
+  process.env.DB_USERNAME || "postgres",
+  process.env.DB_PASSWORD || "docker",
+  {
+    dialect: "postgres",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT) || 5432,
+  }
+);
+
+// Test the database connection
+async function testDatabaseConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+testDatabaseConnection();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`);
+})
+
+app.use("/api", dataRoutes);
 
 // ts-node src/backend/src/index.ts
